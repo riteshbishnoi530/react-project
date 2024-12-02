a7f1709687352103ca570721cec00bc2e2ef9b5c
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-02 11:06:46 +0530

    dropdown

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -4,7 +4,7 @@ const Features = () => {
   return (
     <div className='py-20 flex-wrap flex gap-5 justify-center px-3 group '>
       {SliderTwo_Card.map((card, index) => (
-        <div key={index} className='w-[389px] h-[712px] border rounded-lg p-5 overflow-hidden bg-[#fff] hover:bg-[#FF5501]'>
+        <div key={index} className='w-[389px] h-[712px] border rounded-lg p-5 overflow-hidden hover:bg-[#FF5501] transition-all ease-linear duration-300 bg-[#fff] parent-card'>
           <div className=''>
             <h2 className='font-bold lg:text-[22px] text-lg lg:mb-[11px] mb-2'>{card.headingTwo}</h2>
             <h2 className='text-[#64748B] lg:mb-5 mb-3 lg:text-base text-sm'>{card.paraTwo}</h2>
@@ -12,7 +12,7 @@ const Features = () => {
               <p className='font-semibold xl:text-[56px] lg:text-4xl text-3xl'>{card.money}</p>
               <p className='font-light text-[#4B5768] pl-2'>{card.month}</p>
             </div>
-            <button className='max-w-[341px] text-[#FF5501] hover:border-white hover:text-white duration-300 ease-linear hover:bg-[#FF5501] border-[#FF5501] border-[1px] lg:py-[11px] py-2 lg:text-base text-sm font-semibold rounded-[4px] w-full xl:mb-10 lg:mb-8 mb-6'>{card.getBtn}</button>
+            <button className='max-w-[341px] text-[#FF5501] bg-white border-[#FF5501] border-[1px] lg:py-[11px] py-2 lg:text-base text-sm font-semibold rounded-[4px] w-full xl:mb-10 lg:mb-8 mb-6'>{card.getBtn}</button>
             {card.items.map((item, itemIndex) => (
               <div key={itemIndex} className='gap-4 space-y-2 text-base mb-2 flex'>
                 {item.ImgTwo}
