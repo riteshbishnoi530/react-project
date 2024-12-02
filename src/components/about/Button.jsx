cbcf6ed64b13ec1d43bccc83b5457215da557c8f
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-02 15:21:49 +0530

    add link

--- src/components/about/Button.jsx
+++ src/components/about/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button() {
   return (
     <div className='flex gap-7 mt-9'>
-        <button className='text-[#43BBFF] text-base bg-white rounded-[58px] py-[14px] px-7 font-semibold'>Sign up</button>
+        <a href='https://react-project-sandy-omega.vercel.app/form' className='text-[#43BBFF] text-base bg-white rounded-[58px] py-[14px] px-7 font-semibold'>Sign up</a>
         <button className='text-white border text-base bg-transparent rounded-[58px] py-[14px] px-7 font-semibold'>login</button>
     </div>
   )
