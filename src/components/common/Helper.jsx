5371c34d564c56e3f21b1b6c0aaa84028791b962
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-02 10:08:08 +0530

    cards

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,4 +1,6 @@
-import partner from '../../assets/image/svg/partnership.svg'
+import partner from '../../assets/image/svg/partnership.svg';
+import { TickImg } from './Icons';
+import { CrossImg } from './Icons';
 export const SLIDER_CARDS =[
     {
         imgSrc: partner,
@@ -43,3 +45,57 @@ export const CARDS =[
         paragraph: 'We have developed, capitalized, advised, and invested in $XXXmm of renewable energy projects over our careers. Read more',
     }
 ]
+export const SliderTwo_Card = [
+    {
+        headingTwo: 'Freebie',
+        paraTwo: 'Ideal for individuals who need quick access to basic features.',
+        money: "$0",
+        month: '/ Month',
+        getBtn: 'Get Started Now',
+        items: [
+            { img: <TickImg/>, name: "20,000+ of PNG & SVG graphics" },
+            { img: <TickImg />, name: "Access to 100 million stock images" },
+            { img: <CrossImg />, name: "Upload custom icons and fonts" },
+            { img: <CrossImg />, name: "Unlimited Sharing" },
+            { img: <CrossImg />, name: "Upload graphics & video in up to 4k" },
+            { img: <CrossImg />, name: "Unlimited Projects" },
+            { img: <CrossImg />, name: "Instant Access to our design system" },
+            { img: <CrossImg />, name: "Create teams to collaborate on designs" },
+        ],
+
+    },
+    {
+        headingTwo: 'Professional',
+        paraTwo: 'Ideal for individuals who who need advanced features and tools for client work.',
+        money: "$25",
+        month: '/ Month',
+        getBtn: 'Get Started Now',
+        items: [
+            { img: <TickImg />, name: "20,000+ of PNG & SVG graphics" },
+            { img: <TickImg />, name: "Access to 100 million stock images" },
+            { img: <TickImg />, name: "Upload custom icons and fonts" },
+            { img: <TickImg />, name: "Unlimited Sharing" },
+            { img: <TickImg />, name: "Upload graphics & video in up to 4k" },
+
+        ],
+
+    },
+    {
+        headingTwo: 'Enterprise',
+        paraTwo: 'Ideal for businesses who need personalized services and security for large teams. ',
+        money: "$100",
+        month: '/ Month',
+        getBtn: 'Get Started Now',
+        items: [
+            { img: <TickImg />, name: "20,000+ of PNG & SVG graphics" },
+            { img: <TickImg />, name: "Access to 100 million stock images" },
+            { img: <TickImg />, name: "Upload custom icons and fonts" },
+            { img: <TickImg />, name: "Unlimited Sharing" },
+            { img: <TickImg />, name: "Upload graphics & video in up to 4k" },
+            { img: <TickImg />, name: "Unlimited Projects" },
+            { img: <TickImg />, name: "Instant Access to our design system" },
+            { img: <TickImg />, name: "Create teams to collaborate on designs" },
+        ],
+
+    },
+];
--- src/components/view/Home.jsx
+++ src/components/view/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../common/Hero'
 import Slick from '../home/Slick'
 import Footer from '../common/Footer'
+import Features from '../home/Features'
 
 function Home() {
   return (
     <div>
       <Hero/>
       <Slick/>
+      <Features/>
       <Footer/>
     </div>
   )
