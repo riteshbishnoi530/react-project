c5f471244256c76162a4eb4ed37750c51142a729
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-03 12:34:46 +0530

    use of if else

--- src/components/effect/Parctise.jsx
+++ src/components/effect/Parctise.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react'
 const Parctise = () => {
     const [count, setCount] = useState(0);
-    useEffect(() =>{
-        setTimeout(() =>{
-            setCount(count+2)
-        },1000)
+    useEffect(() => {
+        setTimeout(() => {
+            if (count < 10) {
+                setCount(count + 2);
+            } else {
+                setCount(0);
+            }
+        }, 1000)
     })
-    
-  return (
-    <>
-    <h2 className='text-center font-semibold text-2xl text-red-800 py-10'><span className='text-black'>Counter</span> {count}</h2>
-    </>
-  )
+
+    return (
+        <>
+            <h2 className='text-center font-semibold text-2xl text-red-800 py-10'><span className='text-black'>Counter</span> {count}</h2>
+        </>
+    )
 }
 
 export default Parctise
